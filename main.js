bc23c34a3fd75c31a6effb5fab156c479451ac68
Lucas Guimarães Santos <lucasantos@fh.com.br>
2021-02-08 22:16:49 -0300

    Changing title and content

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
 const { app, BrowserWindow, Menu } = require("electron");
 
+var mainWindow = null;
 //Janela principal
 async function createWindow() {
-  let mainWindow = null;
   let indexMainFile = `${__dirname}/src/pages/editor/index.html`;
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
   });
 
   await mainWindow.loadURL(indexMainFile);
+  // mainWindow.webContents.openDevTools();
+
+  createNewFile()
+}
+
+var file = {};
+//criar novo arquivo
+function createNewFile() {
+  file = {
+    name: "newFile.txt",
+    content: "",
+    saved: false,
+    path: app.getPath("documents") + "/newFile.txt",
+  };
+
+  mainWindow.webContents.send("set-file", file);
 }
 
 //template menu
@@ -23,6 +42,9 @@ const templateMenu = [
         submenu: [
           {
             label: "File",
+            click() {
+              createNewFile();
+            },
           },
         ],
       },
@@ -36,9 +58,9 @@ const templateMenu = [
         label: "Salvar como...",
       },
       {
-        label: 'Sair',
-        role:'quit'
-      }
+        label: "Sair",
+        role: "quit",
+      },
     ],
   },
 ];
---
+++ src/pages/editor/scripts.js
@@ -0,0 +1,10 @@
+const { ipcRenderer } = require("electron");
+
+const title = document.getElementById("title")
+const textarea = document.getElementById("textEditor")
+
+// chamando evendo set-file criado no main.js
+ipcRenderer.on("set-file", function (event, data) {
+    title.innerHTML = `${data.name} | WDEV Editor`
+    textarea.value = data.content
+});
