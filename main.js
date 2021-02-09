1f27cbb9bdf7b14b8a6818476593482180e9ffdf
Lucas Guimarães Santos <lucasantos@fh.com.br>
2021-02-08 23:06:41 -0300

    salvar e salvar como

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const { app, BrowserWindow, Menu } = require("electron");
+const { app, BrowserWindow, Menu, dialog, ipcMain } = require("electron");
+const fs = require("fs");
+const path = require("path");
+const { resourceUsage } = require("process");
 
 var mainWindow = null;
 //Janela principal
@@ -13,9 +16,13 @@ async function createWindow() {
   });
 
   await mainWindow.loadURL(indexMainFile);
-  // mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools(); 
 
-  createNewFile()
+  createNewFile();
+
+  ipcMain.on("update-content", (event, data) => {
+    file.content = data;
+  });
 }
 
 var file = {};
@@ -29,6 +36,43 @@ function createNewFile() {
   };
 
   mainWindow.webContents.send("set-file", file);
+}
+
+async function saveFile(filePath) {
+  try {
+    fs.writeFileSync(filePath, file.content, (error) => {
+      if (error) throw error;
+      file.saved = true;
+      file.path = filePath;
+      file.name = path.basename(filePath);
+
+      //Chamando o evento outra vez para atualizar o nome na janela do app
+      mainWindow.webContents.send("set-file", file);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function saveFileAs() {
+  let dialogFile = await dialog.showSaveDialog({
+    defaultPath: file.path,
+  });
+
+  if (dialogFile.canceled) {
+    return;
+  }
+  saveFile(dialogFile.filePath);
+}
+
+function justSaveFile() {
+  debugger
+  if (file.saved) {
+    return saveFile(file.path);
+  }
+
+  //salvar como
+  return saveFileAs();
 }
 
 //template menu
@@ -53,9 +97,15 @@ const templateMenu = [
       },
       {
         label: "Salvar",
+        click() {
+          justSaveFile();
+        },
       },
       {
         label: "Salvar como...",
+        click() {
+          saveFileAs();
+        },
       },
       {
         label: "Sair",
--- src/pages/editor/scripts.js
+++ src/pages/editor/scripts.js
@@ -8,3 +8,8 @@ ipcRenderer.on("set-file", function (event, data) {
     title.innerHTML = `${data.name} | WDEV Editor`
     textarea.value = data.content
 });
+
+//Atualizando textarea
+function handleChangeText(){
+    ipcRenderer.send('update-content', textarea.value)
+}
